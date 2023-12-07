8d77d01ad942aad1aeba0b6bb1d103a104c84236
samiulbd22 <97846282+samiulbd22@users.noreply.github.com>
2023-12-07 11:41:16 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ const members = {
         garage_cost     :20,
         running_payable :2784,
         running_paid    :0,
-        running_meal    :6,
+        running_meal    :7,
         // method calling
         previousAddedDining,
         previousDue,
@@ -93,13 +93,13 @@ const members = {
         pre_previous_payable:2500,
         previous_paid       :3000,
         previous_dining_cost:2016,
-        payment             :undefined,
+        payment             :{"06/12/23-Cash":2700},
         // running month
         stay_cost       :2500,
         garage_cost     :0,
         running_payable :4711,
-        running_paid    :0,
-        running_meal    :9,
+        running_paid    :2700,
+        running_meal    :11,
         // method calling
         previousAddedDining,
         previousDue,
@@ -126,7 +126,7 @@ const members = {
         garage_cost     :0,
         running_payable :4603,
         running_paid    :0,
-        running_meal    :7,
+        running_meal    :8,
         // method calling
         previousAddedDining,
         previousDue,
@@ -152,7 +152,7 @@ const members = {
         garage_cost     :0,
         running_payable :3583,
         running_paid    :0,
-        running_meal    :12,
+        running_meal    :14,
         // method calling
         previousAddedDining,
         previousDue,
@@ -179,7 +179,7 @@ const members = {
         garage_cost     :0,
         running_payable :4439,
         running_paid    :6000,
-        running_meal    :8,
+        running_meal    :9,
         // method calling
         previousAddedDining,
         previousDue,
